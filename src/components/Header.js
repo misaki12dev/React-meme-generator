151d0200d2f78e2655fe8484ea063cc5da3e44a5
misaki12dev <misaki.12k@gmail.com>
2022-09-02 18:58:42 +0900

    styling

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="images/face.png" className="header--img"/>
+      <h2 className="header--title">Meme Generator</h2>
+    </header>
+  );
+}
