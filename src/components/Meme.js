db0675615e90e07129e62dee89c9a596c7cc0b5c
misaki12dev <misaki.12k@gmail.com>
2022-09-03 20:43:27 +0900

    show pictures by using useState

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,29 @@
 import React from "react";
+import memesData from "../memeData";
 
 export default function Meme() {
+
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Buttom text" className="form--input" />
-        <button className="form--button">Get a new meme image</button>
-      </form>
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image
+        </button>
+      </div>
+      <div>
+        <img src={memeImage} className="meme--image"/>
+      </div>
     </main>
   );
 }
