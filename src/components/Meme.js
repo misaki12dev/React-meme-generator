b539797b566927a2716b2edad6718627f9c86766
misaki12dev <misaki.12k@gmail.com>
2022-09-04 17:07:34 +0900

    delete comments

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,23 +2,6 @@ import React from "react";
 import memesData from "../memeData";
 
 export default function Meme() {
-  /**
-   * Challenge: Update our state to save the meme-related
-   * data as an object called `meme`. It should have the
-   * following 3 properties:
-   * topText, bottomText, randomImage.
-   *
-   * The 2 text states can default to empty strings for now,
-   * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
-   *
-   * Next, create a new state variable called `allMemeImages`
-   * which will default to `memesData`, which we imported above
-   *
-   * Lastly, update the `getMemeImage` function and the markup
-   * to reflect our newly reformed state object and array in the
-   * correct way.
-   */
-
   const [meme, setMeme] = React.useState({
     randomImage: "http://i.imgflip.com/1bij.jpg",
     topText: "",
